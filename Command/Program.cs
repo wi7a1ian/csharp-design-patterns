2c7ebfa9d1908f6fa6714c41906ee4a896fe522d
Wit <wzajac@krollontrack.pl>
2019-08-06 10:33:14 +0200

    Added sample command pattern

---
+++ Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Command
+{
+    class Program
+    {
+        static Queue<ICommand> eventQueue = new Queue<ICommand>();
+
+        static void Main(string[] args)
+        {
+            eventQueue.Enqueue(new RelayCommand(() => Console.WriteLine("Hi")));
+            eventQueue.Enqueue(new RelayCommand(() => Console.WriteLine("Bye"), () => false));
+
+            var prm = new { someParam = true };
+            eventQueue.AsParallel().Where( x => x.CanExecute(prm)).ForAll( x => x.Execute(prm));
+        }
+    }
+}
---
+++ Command/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace Command
+{
+    /// <summary>
+    /// https://gist.github.com/wi7a1ian/28c042b64cfd26e8e3bb5de64c0d50f6
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute)
+        : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { /*CommandManager.RequerySuggested += value;*/ }
+            remove { /*CommandManager.RequerySuggested -= value;*/ }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+
+    public class RelayCommand : RelayCommand<object>
+    {
+        public RelayCommand(Action execute) : this(execute, null) { }
+        public RelayCommand(Action execute, Func<bool> canExecute) : base(param => execute(), param => (canExecute != null) ? canExecute() : true) { }
+    }
+}
